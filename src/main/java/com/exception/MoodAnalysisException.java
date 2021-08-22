a6b8aaf0aead1ea49bcb6429c088368fc8ef4301
Jay22031999 <Jay22031999>
2021-08-22 18:32:30 +0530

    Use case 2

---
+++ src/main/java/com/exception/MoodAnalysisException.java
@@ -0,0 +1,9 @@
+package src.main.java.com.exception;
+
+public class MoodAnalysisException extends Exception{
+
+    public MoodAnalysisException(String message){
+        super(message);
+    }
+
+}
--- src/test/java/com/exception/MoodAnalyserTest.java
+++ src/test/java/com/exception/MoodAnalyserTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
-	
-	 @Test
-	    public void givenNullMood_ShouldReturnHappy() throws MoodAnalysisException {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-	        String mood = moodAnalyser.analyseMood("This is a Happy message");
-	        Assertions.assertEquals("HAPPY", mood);
 
-	    }
 
+
+    @Test
+    public void givenNullMood_ShouldReturnHappy() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood("This is a Happy message");
+        Assertions.assertEquals("HAPPY", mood);
+
+    }
 }
